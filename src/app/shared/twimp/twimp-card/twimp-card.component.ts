3edb5754257ded424c8c6770d43f8ebab62ad351
nuria.santossanchez <nuria.santossanchez@gmail.com>
2023-07-01 21:37:27 +0200

    TwimpListComponent

---
+++ src/app/shared/twimp/twimp-card/twimp-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'teewmpus-twimp-card',
+  templateUrl: './twimp-card.component.html',
+  styleUrls: ['./twimp-card.component.css']
+})
+export class TwimpCardComponent {
+
+}
