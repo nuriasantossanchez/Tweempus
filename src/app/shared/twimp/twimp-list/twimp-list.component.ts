ec3fb002c693fe52ff5c5ecf311afd59824102d2
nuria.santossanchez <nuria.santossanchez@gmail.com>
2023-07-04 21:02:06 +0200

    data binding @Input()

--- src/app/shared/twimp/twimp-list/twimp-list.component.ts
+++ src/app/shared/twimp/twimp-list/twimp-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Twimp } from '../twimp.model';
+import { Author } from '../../author/author.model';
 
 @Component({
   selector: 'teewmpus-twimp-list',
   templateUrl: './twimp-list.component.html',
-  styleUrls: ['./twimp-list.component.css']
+  styleUrls: ['./twimp-list.component.css'],
 })
-export class TwimpListComponent {
+export class TwimpListComponent implements OnInit {
+  text: string =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam efficitur sodales libero, sit amet  posuere arcu consectetur ut. Nam volutpat ligula ac  nunc consectetur vestibulum.';
+  authors: Author[] = [];
+  twimps: Twimp[] = [];
 
+  ngOnInit() {
+    this.authors.push(new Author('1'));
+    //datos: el array de twimps, seran datos que le lleguen de otro componente, los recibira del dashboard
+    this.twimps.push(new Twimp('1', '', this.authors[0], this.text, '01/01/2023'));
+    this.twimps.push(new Twimp('2', '', this.authors[0], this.text, '01/01/2023'));
+    this.twimps.push(new Twimp('3', '', this.authors[0], this.text, '01/01/2023'));
+    this.twimps.push(new Twimp('4', '', this.authors[0], this.text, '01/01/2023'));
+  }
 }
